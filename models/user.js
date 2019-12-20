9bb8c416cb45b571c43f95356be8c7e9b3b1e569
Chris Glenn <chrisglenn519@yahoo.com>
2019-12-19 20:10:08 -0600

    package

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ module.exports = function(sequelize, DataTypes) {
   });
 
   User.associate = function(models) {
-    User.belongsTo(models.Account);
+    User.hasOne(models.Account);
   };
   return User;
 };
