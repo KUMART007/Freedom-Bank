6f9bc77c94f2229fba3b3fd6dae84471764b4c13
Chris Glenn <chrisglenn519@yahoo.com>
2019-12-19 21:15:05 -0600

    fixed webpage on startup

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,57 +1,52 @@
-/*
 var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+    res.render("layouts/main");
   });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
-};*/
+};
+
+// app.get("/example/:id", function(req, res) {
+//   db.Example.findOne({ where: { id: req.params.id } }).then(function(
+//     dbExample
+//   ) {
+//     res.render("example", {
+//       example: dbExample
+//     });
+//   });
+// });
 
 // Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
-//
-// Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
-//
-module.exports = function (app) {
-  //
-  app.get("/", function (req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
-  //
-  app.get("/login", function (req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
+// var path = require("path");
+// //
+// // Requiring our custom middleware for checking if a user is logged in
+// var isAuthenticated = require("../config/middleware/isAuthenticated");
+// //
+// module.exports = function (app) {
+//   //
+//   app.get("/", function (req, res) {
+//     // If the user already has an account send them to the members page
+//     if (req.user) {
+//       res.redirect("/members");
+//     }
+//     res.sendFile(path.join(__dirname, "../public/login.html"));
+//   });
+//   //
+//   app.get("/login", function (req, res) {
+//     // If the user already has an account send them to the members page
+//     if (req.user) {
+//       res.redirect("/members");
+//     }
+//     res.sendFile(path.join(__dirname, "../public/login.html"));
+//   });
 
-  app.get("/members", isAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
-};
+//   app.get("/members", isAuthenticated, function (req, res) {
+//     res.sendFile(path.join(__dirname, "../public/members.html"));
+//   });
+// };
